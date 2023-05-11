97f6f7febe3e622864e152791c25d43777979f87
DMMutua <lengowriter99@gmail.com>
2023-05-11 18:26:15 +0300

    storing the history of inputs and outputs of a particular function

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -7,20 +7,36 @@ Caching Functions and Methods"""
 import redis
 import uuid
 from typing import Union, Callable, Optional, Any, TypeVar
-from functools import wraps
+import functools
 
 T = TypeVar('T')
 
 
 def count_calls(func: Callable) -> Callable:
-    """A Decorator that Counts Number of Times a Method is called."""
+    """A Decorator that Counts
+    Number of Times a Method is called."""
 
-    @wraps(func)
-    def wrapper(*args, **kwargs):
+    @functools.wraps(func)
+    def wrapper(self, *args, **kwargs):
         key = func.__qualname__
-        self.redis.incr(key)
-        return func(*args, **kwargs)
+        self._redis.incr(key)
+        call = pickle.dumps((args, func(self, *args, **kwargs)))
+        self._redis.rpush(key, call)
+        return func(self, *args, **kwargs)
+    return wrapper
 
+def call_history(method: Callable) -> Callable:
+    """A Decorator that Stores the Call
+    History of Cache Methods"""
+
+    @functools.wraps(method)
+    def wrapper(self, *args, **kwargs):
+        key_inputs = "{}:inputs".format(method.__qualname__)
+        key_outputs = "{}:outputs".format(method.__qualname__)
+        self._redis.rpush(key_inputs, str(args))
+        output = method(self, *args, **kwargs)
+        self._redis.rpush(key_outputs, output)
+        return output
     return wrapper
 
 
@@ -33,6 +49,7 @@ class Cache:
         self._redis = redis.Redis()
         self._redis.flushdb()
 
+    @call_history
     @count_calls
     def store(self, data: Union[str, bytes, int, float]) -> str:
         """Type Annotated Method to take `data` arg
